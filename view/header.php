e2182bbb7f8a9433e9c018802752cf95a5d1730f
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-11-21 16:29:24 -0600

    Update header.php

--- view/header.php
+++ view/header.php
@@ -1,1 +1,37 @@
-
+<!doctype html>
+<html lang="en" data-bs-theme="dark">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> <?=$PageTitle?> </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class = "container">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/">Example</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/">Example</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/">Example</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/">Example</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
