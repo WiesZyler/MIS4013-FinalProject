3e99110300a554f7303d74dbfcad89fd661a68c7
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-11-26 13:34:50 -0600

    Update pack.php

--- Models/pack.php
+++ Models/pack.php
@@ -5,8 +5,20 @@ function selectPack() {
         $stmt = $conn->prepare("SELECT PackID, PName, PReleaseDate FROM `pack`");
         $stmt->execute();
         $result = $stmt->get_result();
-        $conn->close();
-        return $result;
+        
+        if ($result->num_rows > 0) {
+            $data = [];
+            while ($row = $result->fetch_assoc()) {
+                $data[] = $row;
+            }
+            $conn->close();
+            header('Content-Type: application/json');
+            echo json_encode($data);
+        } else {
+            $conn->close();
+            header('Content-Type: application/json');
+            echo json_encode([]);
+        }
     } catch (Exception $e) {
         $conn->close();
         throw $e;
