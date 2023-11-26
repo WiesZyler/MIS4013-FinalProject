7341bd98caeb37dd64c2a696dc4c7e33a8370364
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-11-26 13:30:26 -0600

    Update index.php

--- Views/Pack/index.php
+++ Views/Pack/index.php
@@ -1,20 +1,35 @@
 <div id="table"></div>
 <script>
   {
+    var grid = null;
+    var g;
     async function ShowTable()
       {
-    let r = await fetch( '<?php selectPack ?>' );
+let tbl = document.querySelector("#table");
+        tbl.innerHTML = "";
+    let r = await fetch( '<?php selectPack() ?>', { method: "post", cache: "no-store"} );
     let rj = await r.json();
+
+    let params = {
+      data: rj,
+      search: true,
+      sort: true,
+      width: 800,
+      pagination: {limit: 10,},
+    };
  
   
-const grid = new Grid({
-  data: rj
-});
+grid = new gridjs.Grid(params);
+    grid.render(tbl);
+
+        
       }
-
-
+ShowTable();
   }
 
+         
+       
 
+  
   
 </script>
