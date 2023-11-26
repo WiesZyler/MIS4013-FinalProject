a6815972b90e4e09559f0330571930a085bb3991
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-11-26 13:38:53 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 $PageTitle = "Home";
-include "views/header.php";
+include "Views/header.php";
 ?>
     <h1>Final Project</h1>
 
 <?php
-include "views/footer.php";
+include "Views/footer.php";
 ?>
