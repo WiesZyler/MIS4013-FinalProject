b2b874b118a1d05d80cfe1584ffcfe0b7d9d482f
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-11-26 12:59:50 -0600

    Update index.php

--- Views/Pack/index.php
+++ Views/Pack/index.php
@@ -1,1 +1,11 @@
+<div id="table"></div>
+<script>
 
+const grid = new Grid({
+  data: [
+    ['John', 'john@example.com', '(353) 01 222 3333'],
+    ['Mark', 'mark@gmail.com',   '(01) 22 888 4444']
+  ]
+});
+  
+</script>
