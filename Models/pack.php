899d86727894e810e8b533085eea83c6cad752e3
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-11-26 12:13:19 -0600

    Update pack.php

--- Models/pack.php
+++ Models/pack.php
@@ -1,1 +1,15 @@
-
+<?php
+function selectPack() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT P{ackID, PName, PReleaseDate FROM `pack`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
