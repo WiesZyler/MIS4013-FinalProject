dc82ee35745f3c1c322672eb0392d77e475b4bbf
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-11-27 14:16:11 -0600

    Update header.php

--- Views/header.php
+++ Views/header.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en" data-bs-theme="dark">
+<html lang="en" >
   <head>
       <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
