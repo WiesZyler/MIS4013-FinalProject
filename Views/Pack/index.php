0bcb8d91ba8912ddeb8b7860e01ad2a19bb6d677
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-11-27 13:53:09 -0600

    Update index.php

--- Views/Pack/index.php
+++ Views/Pack/index.php
@@ -1,15 +1,13 @@
-
 <script>
 const grid = new Grid({
   columns: ['Pack ID', 'Pack Name', 'Release Date'],
   data: [
-   <?php While($pack = $packs->fetch_assoc())
-     {
-     echo 
-       "['".$Pack['PackID']."', '". $Pack['PName'] . "', '" . Pack['PReleaseDate']."'],"
-    
-       }
-?>
+    <?php 
+    while ($pack = $packs->fetch_assoc()) {
+      echo "['" . $pack['PackID'] . "', '" . $pack['PName'] . "', '" . $pack['PReleaseDate'] . "'],";
+    }
+    ?>
   ]
 });
 </script>
+
