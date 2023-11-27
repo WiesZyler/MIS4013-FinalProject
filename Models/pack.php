1019f228e350e52676a9b3e26382de8b59bf90ba
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-11-27 13:36:51 -0600

    Update pack.php

--- Models/pack.php
+++ Models/pack.php
@@ -2,23 +2,11 @@
 function selectPack() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT PackID, PName, PReleaseDate FROM `Pack`");
+        $stmt = $conn->prepare("SELECT PackID, PName, PReleaseDate FROM `pack`");
         $stmt->execute();
         $result = $stmt->get_result();
-        
-        if ($result->num_rows > 0) {
-            $data = [];
-            while ($row = $result->fetch_assoc()) {
-                $data[] = $row;
-            }
-            $conn->close();
-            header('Content-Type: application/json');
-            echo json_encode($data);
-        } else {
-            $conn->close();
-            header('Content-Type: application/json');
-            echo json_encode([]);
-        }
+        $conn->close();
+        return $result;
     } catch (Exception $e) {
         $conn->close();
         throw $e;
