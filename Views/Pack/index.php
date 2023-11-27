b3268aab8739a2b784fdf198983aea0018590ab1
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-11-27 13:54:58 -0600

    Update index.php

--- Views/Pack/index.php
+++ Views/Pack/index.php
@@ -1,3 +1,8 @@
+  <!-- Data table -->
+    <link href="https://unpkg.com/gridjs/dist/theme/mermaid.min.css" rel="stylesheet" />
+    <script src="https://unpkg.com/gridjs/dist/gridjs.umd.js"></script>
+
+
 <script>
 const grid = new Grid({
   columns: ['Pack ID', 'Pack Name', 'Release Date'],
