e3be189b5c03282bfd305a4d5b1c4adc436483fe
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-11-27 14:35:21 -0600

    Update index.php

--- Views/Pack/index.php
+++ Views/Pack/index.php
@@ -1,23 +1,10 @@
   <!-- Data table -->
     <link href="https://unpkg.com/gridjs/dist/theme/mermaid.min.css" rel="stylesheet" />
     <script src="https://unpkg.com/gridjs/dist/gridjs.umd.js"></script>
-<style>
-  .gridjs-search {
-  float: right;
-    
-}
-  .input.gridjs-input{
-    'background-color': '#8b2e2e', 
-  }
-    #tbl > div > div.gridjs-head > div > input{
-     'background-color': '#8b2e2e', 
-    }
-</style>
 <div id="tbl"></div>
 <script>
 const grid = new gridjs.Grid({
   columns: ['Pack ID', 'Pack Name', 'Release Date'],
-  search: true,
   sort: true,
   pagination: true,
   data: [
@@ -45,10 +32,6 @@ const grid = new gridjs.Grid({
     td: {
       'border-bottom': '1px solid #2e2e2e',
       'text-align': 'center',
-    },
-    search: {
-      'background-color': '#8b2e2e', 
-      color: '#378',
     },
     pagination: {
       'background-color': '#2e2e2e',
