b6d3bc5128691c46fbd20e2b39cbb8fb7d91f413
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-11-28 16:03:16 -0600

    Update card.php

--- card.php
+++ card.php
@@ -1,1 +1,13 @@
+<?php
 
+require_once("util-db.php");
+require_once("Models/card.php");
+
+$PageTitle = "Cards";
+include "Views/header.php";
+
+$cards = selectCard();
+include "Views/Card/index.php";
+include "Views/footer.php";
+
+?>
