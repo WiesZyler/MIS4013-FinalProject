9b01a6df09b5d79fc67b602ced5600c61717b25e
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-11-28 15:33:17 -0600

    Update deck.php

--- deck.php
+++ deck.php
@@ -1,1 +1,13 @@
+<?php
 
+require_once("util-db.php");
+require_once("Models/deck.php");
+
+$PageTitle = "Decks";
+include "Views/header.php";
+
+$decks = selectPack();
+include "Views/Deck/index.php";
+include "Views/footer.php";
+
+?>
