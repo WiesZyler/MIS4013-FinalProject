9ab611fc757a9c30e2594e749f50e6e754a41e45
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-11-28 15:56:33 -0600

    Update card.php

--- Models/card.php
+++ Models/card.php
@@ -1,1 +1,16 @@
 
+<?php
+function selectCard() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT CardID, CName, CColorID, CCardType, CRarity FROM `Card`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
