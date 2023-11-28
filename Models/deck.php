4a3cc40d758eb3e37961a00b2036ce17276d4353
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-11-28 15:20:23 -0600

    Update deck.php

--- Models/deck.php
+++ Models/deck.php
@@ -1,1 +1,16 @@
 
+<?php
+function selectDeck() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT DeckID, DName, DFormat, DPlayerName FROM `deck`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
