d6e9bc34f6f9c8ab210d7bc55a0978dbc82aab83
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-11-28 21:27:19 -0600

    Update index.php

--- Views/Store/index.php
+++ Views/Store/index.php
@@ -1,1 +1,43 @@
-
+  <!-- Data table -->
+    <link href="https://unpkg.com/gridjs/dist/theme/mermaid.min.css" rel="stylesheet" />
+    <script src="https://unpkg.com/gridjs/dist/gridjs.umd.js"></script>
+<div id="tbl"></div>
+<script>
+const grid = new gridjs.Grid({
+  columns: ['Store ID', 'Store Name', 'Latitude, Longitude'],
+  sort: true,
+  pagination: true,
+  data: [
+    <?php 
+    while ($store = $stores->fetch_assoc()) {
+      echo "['" . $store['StoreID'] . "', '" . $store['SName'] . "', '" . $store['SLat'] "', '" . $store['SLon'] . "'],";
+    }
+    ?>
+  ],
+  style: {
+    grid: {
+      'background-color': '#1e1e1e',
+      color: '#ddd',
+      'font-family': 'Arial, sans-serif',
+    },
+    table: {
+      border: '1px solid #2e2e2e',
+    },
+    th: {
+      'background-color': '#2e2e2e',
+      color: '#ddd',
+      'border-bottom': '1px solid #2e2e2e',
+      'text-align': 'center',
+    },
+    td: {
+      'border-bottom': '1px solid #2e2e2e',
+      'text-align': 'center',
+    },
+    pagination: {
+      'background-color': '#2e2e2e',
+      color: '#ddd',
+    },
+  }
+});
+  grid.render(tbl);
+</script>
