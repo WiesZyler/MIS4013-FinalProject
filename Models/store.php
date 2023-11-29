6b8d9405f52e27c93d799720bffdf0e67a82d824
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-11-28 21:24:15 -0600

    Update store.php

--- Models/store.php
+++ Models/store.php
@@ -1,1 +1,15 @@
-
+<?php
+function selectStore() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT StoreID, SName, SLat, SLon FROM `Store`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
