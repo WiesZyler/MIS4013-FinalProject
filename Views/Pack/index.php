2eef9a58b012362b0355d8068cf701c45db0a0b2
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-11-30 16:59:08 -0600

    Update index.php

--- Views/Pack/index.php
+++ Views/Pack/index.php
@@ -3,17 +3,33 @@
     <script src="https://unpkg.com/gridjs/dist/gridjs.umd.js"></script>
 <div id="tbl"></div>
 <script>
+const packData = [
+  <?php 
+  while ($pack = $packs->fetch_assoc()) {
+    echo "['" . $pack['PackID'] . "', '" . $pack['PName'] . "', '" . $pack['PReleaseDate'] . "'],";
+  }
+  ?>
+];
+
 const grid = new gridjs.Grid({
   columns: ['Pack ID', 'Pack Name', 'Release Date'],
   sort: true,
-  pagination: {limit:10},
-  data: [
-    <?php 
-    while ($pack = $packs->fetch_assoc()) {
-      echo "['" . $pack['PackID'] . "', '" . $pack['PName'] . "', '" . $pack['PReleaseDate'] . "'],";
-    }
-    ?>
-  ],
+  pagination: { limit: 10 },
+  data: packData.map(data => ({
+    'Pack ID': data[0],
+    'Pack Name': data[1],
+    'Release Date': data[2],
+    'Action': `<form method="post" action="">
+                  <input type="hidden" name="pID" value="${data[0]}">
+                  <input type="hidden" name="actionType" value="Delete">
+                  <button type="Submit" class="btn" onclick="return confirm('Are You Sure?');"> 
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+                      <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+                    </svg>
+                  </button>
+                </form>`,
+  })),
   style: {
     grid: {
       'background-color': '#1e1e1e',
