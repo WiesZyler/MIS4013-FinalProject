67c050f01ccc9a80a74005a583e68fcaf1b20213
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-11-30 23:42:07 -0600

    Update index.php

--- Views/Pack/index.php
+++ Views/Pack/index.php
@@ -16,7 +16,7 @@
            <label class="input-group-text">Release Date</label>
            <input type="date" id="rdate" class="form-control" />
       </div>
-      <div class="btn-group" style="margin-top:5px; width:200px;" >
+      <div class="btn-group" style="margin-top:5px; width:200px; display:flex; flex-direction:row; align-items:center; justify-content:center;" >
            <button class="btn btn-primary btn-sm" id="addbtn">Add</button>
            <button class="btn btn-warning btn-sm" id="editbtn">Edit</button>
 	   <button class="btn btn-danger btn-sm" id="deletebtn">Delete</button>
