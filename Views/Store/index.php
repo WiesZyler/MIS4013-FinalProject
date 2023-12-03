6f4f888341e2149b429411af260d7c1c5a0a78a1
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-12-03 13:43:45 -0600

    Update index.php

--- Views/Store/index.php
+++ Views/Store/index.php
@@ -6,29 +6,34 @@
 
 <!-- Modal -->
 <div id="myModal" class="modal">
-  <div class="modal-content" style="width: 400px; display: flex; flex-direction: column; align-items: center;">  	  
-   <button id="closeModalBtn" class="btn btn-sm" style="position: absolute; top: 5px; right: 5px;">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
-        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
-      </svg>
-    </button>
-    <span style="font-size: 20px; font-weight: bold;"></p>Store Information</span>
-    <form method="post" action="">
-    <input type="hidden" name="sid" id="sid">
-    <div class="input-group">
-      <label class="input-group-text">Store Name</label>
-      <input type="text" id="sname" name="sname" class="form-control" />
-    </div>
-    <div class="input-group">
-      <label class="input-group-text">Latitude</label>
-      <input type="text" id="slat" name="slat" class="form-control" />
-    </div>
-
-     <div class="input-group">
-      <label class="input-group-text">Longitude</label>
-      <input type="text" id="slon" name="slon" class="form-control" />
-    </div>
+  <div class="modal-content" style="width: 1000px; display: flex; flex-direction: column; align-items: center;"> 
+	<div>
+		<div>
+			<button id="closeModalBtn" class="btn btn-sm" style="position: absolute; top: 5px; right: 5px;">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+                          </svg>
+                        </button>
+                        <span style="font-size: 20px; font-weight: bold;"></p>Store Information</span>
+                        <form method="post" action="">
+                           <input type="hidden" name="sid" id="sid">
+                           <div class="input-group">
+                              <label class="input-group-text">Store Name</label>
+                              <input type="text" id="sname" name="sname" class="form-control" />
+                           </div>
+                           <div class="input-group">
+                              <label class="input-group-text">Latitude</label>
+                              <input type="text" id="slat" name="slat" class="form-control" />
+                           </div>
+                           <div class="input-group">
+                              <label class="input-group-text">Longitude</label>
+                              <input type="text" id="slon" name="slon" class="form-control" />
+                          </div>
+		</div>
+			  <div id="mapdiv" style="width:600px; height:400px; border-radius:10px; margin-left:5px;">MAP</div>
+	</div>
+   
     
     <input type="hidden" name="actionType" id="actionType" value="">
     <div class="btn-group" style="margin-top: 5px; width: 200px; display: flex; justify-content: center;">
@@ -52,6 +57,10 @@ var sname = document.querySelector("#sname");
 var slat = document.querySelector("#slat");
 var slon = document.querySelector("#slon");
 var actionType = document.querySelector("#actionType");
+
+var marker = null
+let mapdiv = document.querySelector("#mapdiv")
+mapdiv.innerHTML = "";
 
 // event listeners for modal buttons
 let addbtn = document.querySelector("#addbtn");
@@ -89,6 +98,52 @@ function closeModal() {
   modal.style.display = 'none';
 }
 
+
+
+var map = L.map('mapdiv', { attributionControl: false, }).setView([slat.value, slon.value], 12);
+map.on("click",(e)=>{
+				g = e;
+				let lati=e.latlng.lat;
+				let longi=e.latlng.lng;
+
+					slat.value = lati;
+					slon.value = longi;
+					if (marker != null) {
+						map.removeLayer(marker);
+						marker = null;
+					}
+
+					marker = L.marker([lati, longi]);
+					map.setView([lati, longi], 12);
+
+					marker.bindPopup(sname.value);
+
+					marker.addTo(map);
+					marker.openPopup();
+
+					
+				})
+
+				if (marker != null) {
+					map.removeLayer(marker);
+					marker = null;
+				}
+	marker = L.marker([slat.value, slon.value]);
+			
+
+				marker.bindPopup(sname.value);
+	
+				marker.addTo(map);
+				marker.openPopup();
+
+				let tile = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+					maxZoom: 20,
+					subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+				});
+				tile.addTo(map);
+
+
+	
 // Table creation
 const grid = new gridjs.Grid({
   columns: ['Store ID','Store Name', 'Latitude', 'Longitude'],
