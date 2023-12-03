52758227cab30f3e4a6a87dc846109a9b793b7f2
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-12-03 12:46:03 -0600

    Update store.php

--- Models/store.php
+++ Models/store.php
@@ -12,4 +12,49 @@ function selectStore() {
         throw $e;
     }
 }
+
+function insertStore($sname,$lon,$slat) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `Store` (`SName`, `SLon`, `SLat`) VALUES (?, ?, ?)");
+         $stmt->bind_param("sss",$sname,$lon,$slat);
+      $success =  $stmt->execute();
+
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteStore($sid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("Delete from `Store` where StoreID=?");
+         $stmt->bind_param("i", $sid);
+      $success =  $stmt->execute();
+
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function editStore($sname,$lon,$slat) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `Store` SET `SName`=?, `SLon`=?, `SLat`=? WHERE `StoreID`=?");
+         $stmt->bind_param("sss",$sname,$lon,$slat);
+      $success =  $stmt->execute();
+
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
