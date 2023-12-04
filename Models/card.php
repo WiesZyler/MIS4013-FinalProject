8620afffaa5ccf04699baf07773e5ec3796a638a
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-12-03 20:03:27 -0600

    Update card.php

--- Models/card.php
+++ Models/card.php
@@ -13,4 +13,57 @@ function selectCard() {
         throw $e;
     }
 }
+
+
+function insertCard($cname,$ccolorid,ccardtype,crarity) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `Card` (`CName`, `CColorID`, `CCardType`, `CRarity`) VALUES (?, ?,?,?)");
+         $stmt->bind_param("siss",$cname,$ccolorid,ccardtype,crarity);
+      $success =  $stmt->execute();
+
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteCard($cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("Delete from `Card` where CardID=?");
+         $stmt->bind_param("s", $cid);
+      $success =  $stmt->execute();
+
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function editCard($cname,$ccolorid,ccardtype,crarity,$cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `Card` SET `CName`=?, `CColorID`=?, `CCardType`=?, `CRarity`=? WHERE `CardID`=?");
+         $stmt->bind_param("sisss",$cname,$ccolorid,ccardtype,crarity,$cid);
+      $success =  $stmt->execute();
+
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
+
+
+
+
+
+
+
