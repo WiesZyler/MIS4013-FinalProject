b680232ce6db9d690ef737ffa3d2e5443142cbfb
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-12-03 18:38:01 -0600

    Update deck.php

--- deck.php
+++ deck.php
@@ -6,6 +6,70 @@ require_once("Models/deck.php");
 $PageTitle = "Decks";
 include "Views/header.php";
 
+
+if (isset($_POST['actionType']))
+{
+  switch ($_POST['actionType'])
+    {
+      case  "Add":
+      if (insertDeck($_POST['dname'],$_POST['dformat'],$_POST['dplayername']))
+      {
+         ?>
+       <script>
+         Swal.fire({title: "Success", text: "Deck Added!", icon: "success",});
+      </script>
+        <?php
+          }
+      else
+      {
+         ?>
+       <script>
+         Swal.fire({title: "Error", text: "Deck Not Added!", icon: "error",});
+      </script>
+        <?php
+      }
+      break;
+      case  "Delete":
+      if (deleteDeck($_POST['did']))
+      {
+         ?>
+       <script>
+         Swal.fire({title: "Success", text: "Deck Deleted!", icon: "success",});
+      </script>
+        <?php
+          }
+      else
+      {
+        ?>
+       <script>
+         Swal.fire({title: "Error", text: "Deck Not Deleted!", icon: "error",});
+      </script>
+        <?php
+      }
+        break;
+       case  "Edit":
+      if (editDeck($_POST['dname'],$_POST['dformat'],$_POST['dplayername'],$_POST['did']))
+      {
+        ?>
+       <script>
+         Swal.fire({title: "Success", text: "Deck Edited!", icon: "success",});
+      </script>
+        <?php
+          }
+      else
+      {
+         ?>
+       <script>
+         Swal.fire({title: "Error", text: "Deck Not Edited!", icon: "error",});
+      </script>
+        <?php
+      }
+      break;
+    }
+
+}
+
+
 $decks = selectDeck();
 include "Views/Deck/index.php";
 include "Views/footer.php";
