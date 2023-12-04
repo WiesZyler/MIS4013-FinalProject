57b6168c8ec00452fb387fe37745d0301a065e22
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-12-04 14:18:53 -0600

    Update header.php

--- Views/header.php
+++ Views/header.php
@@ -3,7 +3,7 @@
   <head>
 
 
-	<link href="bootstrap.css" rel="stylesheet"/>
+	<link src="bootstrap.css" rel="stylesheet"/>
 	  
       <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
