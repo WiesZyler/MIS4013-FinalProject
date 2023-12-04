51d8963a80e73a73c0e276d62aae10f628d4716e
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-12-03 19:30:00 -0600

    Update index.php

--- Views/Deck/index.php
+++ Views/Deck/index.php
@@ -3,42 +3,6 @@
     <link href="https://unpkg.com/gridjs/dist/theme/mermaid.min.css" rel="stylesheet" />
     <script src="https://unpkg.com/gridjs/dist/gridjs.umd.js"></script>
 
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newDeckModal">
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
-    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-      </svg>
-</button>
-<div class="modal fade" id="newDeckModal" tabindex="-1" aria-labelledby="newDeckModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="newDeckModalLabel">New Deck</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <form method="post" action="">
-              <div class="mb-3">
-               <label class="input-group-text">Deck Name</label>
-      		<input type="text" id="dname" name="dname" class="form-control" />	
-              </div>
-              <div class="mb-3">
-              <label class="input-group-text">Format</label>
-      		<input type="text" id="dformat" name="dformat" class="form-control" />
-              </div>
-	   <div class="input-group">
-	      <label class="input-group-text">Player Name</label>
-	      <input type="text" id="dplayername" name="dplayername" class="form-control" />
-	    </div>
-           <input type="hidden" name="actionType" value ="Add">
-              <button type="submit" class="btn btn-primary">Save</button>
-            </form>
-      </div>
-    </div>
-  </div>
-</div>
-
-
 
 
 <!-- Modal -->
@@ -52,7 +16,7 @@
     </button>
     <span style="font-size: 20px; font-weight: bold;"></p>Deck Information</span>
     <form method="post" action="">
-    <input type="hidden" name="did" id="did">
+    <input type="" name="did" id="did">
     <div class="input-group">
       <label class="input-group-text">Deck Name</label>
       <input type="text" id="dname" name="dname" class="form-control" />
