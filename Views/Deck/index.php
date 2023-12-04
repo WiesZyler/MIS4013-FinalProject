5fa3abec5facc81594af15987754f55c7cfa3779
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-12-03 20:42:50 -0600

    Update index.php

--- Views/Deck/index.php
+++ Views/Deck/index.php
@@ -14,6 +14,8 @@
         <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
       </svg>
     </button>
+	  
+<button id="externalAddBtn" class="btn btn-primary">Add Deck</button>
     <span style="font-size: 20px; font-weight: bold;"></p>Deck Information</span>
     <form method="post" action="">
     <input type="hidden" name="did" id="did">
