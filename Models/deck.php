cbf0176d240c18ca3fd6399ffc3bfe72b3e13ae2
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-12-03 18:30:57 -0600

    Update deck.php

--- Models/deck.php
+++ Models/deck.php
@@ -14,3 +14,51 @@ function selectDeck() {
     }
 }
 ?>
+
+
+ <?php
+function insertDeck($dName,$dFormat,$dPlayerName) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `deck` (`dName`, `dFormat`,`dPlayerName`) VALUES (?, ?,?)");
+         $stmt->bind_param("sss",$dName,$dFormat,$dplayerName);
+      $success =  $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteDeck($did) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("Delete from deck where DeckID=?");
+         $stmt->bind_param("i", $did);
+      $success =  $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function editDeck($deckName,$dFormat,$dPlayerName,$dID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `deck` SET `dName`=?, `dFormat`=?,`dPlayerName`=?  WHERE `DeckID`=?");
+        $stmt->bind_param("sssi",$dName,$dFormat,dPlayerName,$did);  
+        $success = $stmt->execute();  
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+
+?>
