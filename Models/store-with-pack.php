0862b14dbd42f47e77dffd7b5a58ace455ef974f
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-12-06 14:04:54 -0600

    Update store-with-pack.php

--- Models/store-with-pack.php
+++ Models/store-with-pack.php
@@ -68,4 +68,18 @@ function selectPack() {
         throw $e;
     }
 }
+
+function selectStore() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT StoreID, SName, SLat, SLon FROM `Store`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
