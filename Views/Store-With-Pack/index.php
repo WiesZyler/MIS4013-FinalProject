8a9cd9985a0c898562239cb490593418b858c39c
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-12-10 17:48:26 -0600

    Update index.php

--- Views/Store-With-Pack/index.php
+++ Views/Store-With-Pack/index.php
@@ -151,35 +151,6 @@ packDropdown.style.display = "none";
 storeDropdown.style.display = "none";
 optionDropdown.addEventListener("change", toggleDropdowns);
 toggleDropdowns();
-
-var tableData;
-var colums;
-function ShowTable(){
-
-	switch(optionDropdown.value){
-		case '1':
-		<?php $OptionSID ?> = storeDropdown.value
-		tableData = [
-			    <?php while ($pack = $packs->fetch_assoc()) 
-                           { 
-					   if (OptionsSID =  $store['StoreID']
-					   echo "['" . $store['StoreID'] . "', '" . $store['SName'] . "'],"; }?>
-			   ];
-		colums =  ['Pack ID', 'Pack Name', 'Release Date'];
-		break;
-
-			
-	}
-
-}
-
-
-
-
-
-
-	
-
 	
 // Table creation
 const grid = new gridjs.Grid({
