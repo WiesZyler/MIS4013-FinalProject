3c6f0927f6156cbee14efe85dd57baeee156ec87
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-12-11 14:22:35 -0600

    Update deck-with-card.php

--- deck-with-card.php
+++ deck-with-card.php
@@ -1,85 +1,79 @@
 <?php
-function selectDeckWithCard() {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("select DC.DCID, D.DeckID, C.CardID, D.DName, DC.DCQuantity, C.CName, C.CRarity, C.CCardType, D.DFormat, DPlayerName from Deck D JOIN DeckCard DC on D.DeckID = DC.DCID JOIN Card C on DC.DCCardID = C.CardID");
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $conn->close();
-        return $result;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
 
-function insertDeck($did, $cid, $quantity) {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("INSERT INTO `DeckCard` (`DCCardID`, `DCDeckID`, `DCQuantity`) VALUES (?, ?, ?)");
-         $stmt->bind_param("iis", $sid, $pid, $quantity);
-      $success =  $stmt->execute();
-        $conn->close();
-        return $success;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
+require_once("util-db.php");
+require_once("Models/deck-with-card.php");
 
-function deleteDeck($dcid) {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("Delete from `DeckCard` where DCID=?");
-         $stmt->bind_param("i", $dcid);
-      $success =  $stmt->execute();
-        $conn->close();
-        return $success;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
+$PageTitle = "Find a Deck";
+include "Views/header.php";
 
-function editDeck($did, $cid, $quantity, $dcid) {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("UPDATE `DeckCard` SET  `DCDeckID`=?, `DCCardID`=?, `DCQuantity`=? WHERE `DCID`=?");
-        $stmt->bind_param("iiii", $did, $cid, $quantity, $dcid);  
-        $success = $stmt->execute();  
-        $conn->close();
-        return $success;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
+if (isset($_POST['actionType']))
+{
+  switch ($_POST['actionType'])
+    {
+      case  "Add":
+      if (insertDeck($_POST['did'],$_POST['cid'],$_POST['quantity']))
+      {
+         ?>
+       <script>
+         Swal.fire({title: "Success", text: "Deck Added!", icon: "success",});
+      </script>
+        <?php
+          }
+      else
+      {
+         ?>
+       <script>
+         Swal.fire({title: "Error", text: "Deck Not Added!", icon: "error",});
+      </script>
+        <?php
+      }
+      break;
+      case  "Delete":
+      if (deleteDeck($_POST['dcid']))
+      {
+         ?>
+       <script>
+         Swal.fire({title: "Success", text: "Deck Deleted!", icon: "success",});
+      </script>
+        <?php
+          }
+      else
+      {
+        ?>
+       <script>
+         Swal.fire({title: "Error", text: "Deck Not Deleted!", icon: "error",});
+      </script>
+        <?php
+      }
+        break;
+       case  "Edit":
+      if (editPack($_POST['did'],$_POST['cid'],$_POST['quantity'],$_POST['dcid']))
+      {
+        ?>
+       <script>
+         Swal.fire({title: "Success", text: "Deck Edited!", icon: "success",});
+      </script>
+        <?php
+          }
+      else
+      {
+         ?>
+       <script>
+         Swal.fire({title: "Error", text: "Deck Not Edited!", icon: "error",});
+      </script>
+        <?php
+      }
+      break;
     }
-}
 
-function selectDeck() {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT DeckID, DName, DFormant,DPlayerName FROM `Deck`");
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $conn->close();
-        return $result;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
 }
+$deckswithcards = selectDeckWithCard();
+$cardswithdecks = selectCardWithDeck();
+$decks = selectDeck();
+$decksopt = selectDeck();
+$cards = selectCards();
+$cardsopt = selectCards();
+include "Views/Deck-With-Card/index.php";
+include "Views/footer.php";
 
-function selectCard() {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT CardID, CName, CColorID, CCardType,CRarity FROM `Card`");
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $conn->close();
-        return $result;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
 ?>
