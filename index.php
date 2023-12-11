798828736de8d92b5b923e439546eb63a1e9278f
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-12-11 14:13:51 -0600

    Update index.php

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ include "Views/header.php";
   </div>
   <div class="col-lg-6">
     <div class="card mb-3 d-flex flex-row" style="background-image:linear-gradient(#17082e 0%, #1a0933 7%, #1a0933 80%, #0c1f4c 100%); color: white; border-style: solid; border-color: #0d1c49;">
-      <img src="img/GS.png" class="card-img-top" alt="Gishath, Sun's Avatar" style="width: 200px;">
+      <img src="img/GS.png" class="card-img-top" alt="Gishath, Sun's Avatar" style="width: 200px; border-radius:4%">
       <div class="card-body" style="max-width: calc(100% - 200px);">
         <h5 class="card-title">Gishath, Sun's Avatar</h5>
         <p class="card-text">Trample, vigilance, haste. Whenever Gishath, Sun's Avatar deals combat damage to a player, reveal that many cards from the top of your library. Put any number of Dinosaur creature cards from among them onto the battlefield and the rest on the bottom of your library in a random order.</p>
@@ -44,7 +44,7 @@ include "Views/header.php";
       <div class="col-md-12">
         <h2>Latest News</h2>
        
-        <div class="card mb-3">
+        <div class="card mb-3" style="background-image:linear-gradient(#17082e 0%, #1a0933 7%, #1a0933 80%, #0c1f4c 100%); color: white; border-style:solid; border-color: #0d1c49;">
           <div class="card-body">
             <h5 class="card-title">How Do the December 2023 MTG Bans Impact Commander?</h5>
             <p class="card-text">We've just seen a fresh handful of bans across three formats: Pioneer, Modern, and Pauper. You might think that this has no impact on Magic's most popular format, but you may actually be surprised!</p>
