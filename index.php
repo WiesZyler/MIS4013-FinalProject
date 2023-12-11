2a79b234d6a5dcbd4c726cffed7873b79d76ca91
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-12-11 14:03:32 -0600

    Update index.php

--- index.php
+++ index.php
@@ -13,12 +13,12 @@ include "Views/header.php";
     </div>
 
     <!-- Featured Cards Section -->
-    <div class="row mt-4">
+   <div class="row mt-4">
   <div class="col-lg-6">
     <h2>Featured Cards</h2>
-    <div class="card mb-3">
+    <div class="card mb-3 d-flex flex-row">
       <img src="img/BL.png" class="card-img-top" alt="Black Lotus" style="width: 200px;">
-      <div class="card-body">
+      <div class="card-body" style="max-width: calc(100% - 200px);">
         <h5 class="card-title">Black Lotus</h5>
         <p class="card-text">Adds 3 mana of any single color of your choice to your mana pool, then is discarded. Tapping this artifact can be played as an interrupt.</p>
         <a href="https://www.tcgplayer.com/product/8687/magic-beta-edition-black-lotus" class="btn btn-primary">View Details</a>
@@ -26,9 +26,9 @@ include "Views/header.php";
     </div>
   </div>
   <div class="col-lg-6">
-    <div class="card mb-3">
+    <div class="card mb-3 d-flex flex-row">
       <img src="img/GS.png" class="card-img-top" alt="Gishath, Sun's Avatar" style="width: 200px;">
-      <div class="card-body">
+      <div class="card-body" style="max-width: calc(100% - 200px);">
         <h5 class="card-title">Gishath, Sun's Avatar</h5>
         <p class="card-text">Trample, vigilance, haste. Whenever Gishath, Sun's Avatar deals combat damage to a player, reveal that many cards from the top of your library. Put any number of Dinosaur creature cards from among them onto the battlefield and the rest on the bottom of your library in a random order.</p>
         <a href="https://www.tcgplayer.com/product/8687/magic-beta-edition-black-lotus" class="btn btn-primary">View Details</a>
@@ -36,6 +36,7 @@ include "Views/header.php";
     </div>
   </div>
 </div>
+
 
 
 
