8fa22abcf960912af51f20ad2ddd0fc0fbfc50d1
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-12-10 18:13:07 -0600

    Update index.php

--- Views/Store-With-Pack/index.php
+++ Views/Store-With-Pack/index.php
@@ -13,15 +13,15 @@
     <select class="form-select" aria-label="Pack Name" id="packOpt">
 		  <option selected>Select A Pack</option>
 		   <?php // PHP loop to collect data from database
-    while ($pack = $packs->fetch_assoc()) {
-    echo '<option value="' . $pack['PackID'] . '">' . $pack['PName'] . '</option>';
+    while ($packopt = $packs->fetch_assoc()) {
+    echo '<option value="' . $packopt['PackID'] . '">' . $packopt['PName'] . '</option>';
 }
     ?>
      </select>
       <select class="form-select" aria-label="Store Name" id="storeOpt">
 	      <?php
-		while ($store = $stores->fetch_assoc()) {
-    echo '<option value="' . $store['StoreID'] . '">' . $store['SName'] . '</option>';
+		while ($storeopt = $stores->fetch_assoc()) {
+    echo '<option value="' . $storeopt['StoreID'] . '">' . $storeopt['SName'] . '</option>';
 }
 
 ?>
